191769165c79208b32b908bdafa65d71ab8b90d9
ryan m <contact@ryanmeek.com>
2019-03-06 22:09:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,6 @@ def key_for_min_value(name_hash)
     name_hash.each do |item|
       if name_hash[prev]
         prev = name_hash[prev] > name_hash[item] ? item : prev
-      else
-        prev = item
       end
     end
     return prev[0]
