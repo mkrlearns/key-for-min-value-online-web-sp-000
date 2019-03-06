0138871721534f2a963472d6553c8ecff246b79d
ryan m <contact@ryanmeek.com>
2019-03-06 22:15:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_key = nil 
   min_value = nil
     name_hash.collect do |key, value|
-     if  value < min_value || !min_value
+     if  value < min_value || min_value == nil
        min_key = value
        min_value = name
      end
